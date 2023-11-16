c1f40add6eed25d9f00d9f93708fbeb0bfdc58cb
anxoaragundi <anxoaragundimesias21@gmail.com>
2023-11-16 18:52:57 +0100

    avanços al codi i afegides les imatges necessàries

--- codi/iniciarPartida.js
+++ codi/iniciarPartida.js
@@ -6,22 +6,27 @@ function iniciarPartida() {
     let files = parseInt(prompt("Introdueix una quantitat de files"));
     let columnes = parseInt(prompt("Introdueix una quantitat de columnes"));
 
-    //revisar que les variables tinguin valors aptes, i aplicar correcions en cas de que no
-    //el valor mínim és 10, i el màxim és 30.
-    if (files < 10) {   //en cas de que files sigui menor que 10
-        files = 10;     //se li assigna el valor 10
-    } else if (files > 30) {    //en cas de que sigui major que 30
-        files = 30              //se li assigna el valor 30
-    }
-
-    //a la variable columnes s'apliquen els mateixos procediments que a la variable files
-    if (columnes < 10) {
-        columnes = 10;
-    } else if (columnes > 30) {
-        columnes = 30
-    }
+    //passar les dues variables per un test que comprova si tenen els tamanys correctes abans d'utilitzar-les
+    esValid(files);
+    esValid(columnes);
 
     //un cop s'han realitzat aquests procediments, es crida a iniciarTaulell() i se li passen les variables
     iniciarTaulell(files, columnes);
 
+    alert("De moment tot bé");
+
+}
+
+
+function esValid(tamany) {
+
+    //revisar que les variables tinguin valors aptes, i aplicar correcions en cas de que no
+    //el valor mínim és 10, i el màxim és 30.
+    if (tamany < 10) {      //en cas de que el tamany sigui menor que 10
+        tamany = 10;        //se li assigna el valor 10
+    } else if (tamany > 30) {   //en cas de que sigui major que 30
+        tamany = 30             //se li assigna el valor 30
+    }
+
+    return tamany;
 }
